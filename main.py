3b3d5aa636a69983275e4c19e30cbc6cbfff82c5
Artem Gavryushenko <artyomgavryeshenko@gmail.com>
2020-09-20 17:38:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def medium_value(array):
+    my_sum = 0
+    for i in array:
+        my_sum += i
+    answer = my_sum / len(array)
+    return answer
+
+
+def dispersion(array):
+    my_sum = 0
+    for i in array:
+        my_sum += i
+    med = medium_value(array)
+
+    answer = 0
+    for i in array:
+        answer += (i - med) ** 2
+    answer = answer / len(array)
+    return answer
+
+
+def median(array):
+    if len(array) % 2 == 1:
+        index = len(array) // 2 + 1
+        return array[index - 1]
+    else:
+        index = len(array) // 2 - 1  # -1 because array indexing
+        return (array[index + 1] + array[index]) / 2
+
+
+def moda(array):
+    mappa = dict.fromkeys(array, 0)
+    for i in array:
+        mappa[i] += 1
+
+    max_of_usages = -1
+    moda_set = []
+    for k, v in mappa.items():
+        if v > max_of_usages:
+            max_of_usages = v
+            moda_set = [k]
+        elif v == max_of_usages:
+            moda_set.append(k)
+
+    return moda_set
+
+
+def max_min(array):
+    max_my = -1
+    min_my = 10 ** 15
+    for i in array:
+        if i > max_my:
+            max_my = i
+        if i < min_my:
+            min_my = i
+
+    return max_my, min_my
+
+
+if __name__ == '__main__':
+    my_set = [1, 2, 2, 3, 4, 5, 6, 7]
+    print(f"{medium_value(my_set)} - medium value")
+    # print(f"{dispersion(my_set)} - dispersion")  # TODO incorrect
+    # TODO стандартне выдхидення
+    print(f"{median(my_set)} - median")
+    print(f"{moda(my_set)} - moda/ modas")
+    print(f"{max_min(my_set)} - max/ min")
+
+    m_m = max_min(my_set)
+    print(f"{m_m[0] - m_m[1]} - rozmax")
+
+    # TODO quantile
---
+++ movie_analysis.py
@@ -0,0 +1,40 @@
+# import csv
+#
+# with open('tmdb_5000_movies.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#
+#     for line in csv_reader:
+#         print(line)
+
+import pandas
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv('tmdb_5000_movies.csv')
+# print(df)
+
+# plotting all number params
+# dataset = pandas.DataFrame(df)
+# # dataset.head()
+# fig = plt.figure(figsize=(8, 8))
+# ax = fig.gca()
+# dataset.hist(ax=ax)
+# plt.show()
+
+min_my = 10 ** 10
+for i in df['vote_count']:
+    if i < min_my:
+        min_my = i
+
+counter = 0
+sum_my = 0
+for i in df['vote_average']:
+    sum_my += i
+    counter += 1
+
+av_vote = sum_my / counter
+
+new_rating = [None] * len(df['title'])
+# for n in df['title']:
+print(df['title'][0].index)
